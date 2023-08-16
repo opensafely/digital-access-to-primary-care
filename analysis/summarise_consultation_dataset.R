fef7bbcd6a720d5e221e933a11bdaa27462ae353
AbodunrinAminu-Manchester <119451273+AbodunrinAminu-Manchester@users.noreply.github.com>
2023-08-16 15:32:43 +0100

    add count has
    To differentiate between number of consultations in total and one occurrence during the period.

--- analysis/summarise_consultation_dataset.R
+++ analysis/summarise_consultation_dataset.R
@@ -13,8 +13,10 @@ df <- arrow::read_feather("output/consultation_dataset.arrow")
 # We could add more variables to group by here, e.g., ethnicity sex
 df_summary <- df %>% 
   dplyr::group_by(patient_age_ge65) %>%
-  dplyr::summarise(count_f2f = sum(count_f2f_consultation, na.rm = TRUE),
-                   count_virtual = sum(count_virtual_consultation, na.rm = TRUE))
+  dplyr::summarise(counttotal_f2f = sum(count_f2f_consultation, na.rm = TRUE),
+                   counttotal_virtual = sum(count_virtual_consultation, na.rm = TRUE),
+                   counthas_f2f = sum(has_f2f_consultation, na.rm = TRUE),
+                   counthas_virtual = sum(has_virtual_consultation, na.rm = TRUE))
 
 # Add start date and end date to the data
 # This currently comes from the dataset definition and this will change once we extract more data
@@ -24,7 +26,7 @@ df_summary <- df_summary %>%
 
 # Pivot data longer so it is easier for further data manipulations or visualisations
 df_summary <- df_summary %>%
-  pivot_longer(cols = c(count_f2f, count_virtual),
+  pivot_longer(cols = c(counttotal_f2f, counttotal_virtual, counthas_f2f, counthas_virtual),
                names_to = c("summary_type", "consultation_type"),
                values_to = "value", names_sep = "_")
 
