cac59436482c5fc7e71914807a6d49cd4e4a288d
AbodunrinAminu-Manchester <119451273+AbodunrinAminu-Manchester@users.noreply.github.com>
2023-10-27 10:09:28 +0100

    Deleted unused codes
    I have removed codes from the R script that were not needed.

--- analysis/summarise_consultation_dataset.R
+++ analysis/summarise_consultation_dataset.R
@@ -23,26 +23,6 @@ consultation_datasets <- consultation_dataset_paths %>%
   dplyr::mutate(file_name = stringr::str_extract(file_name, regex_get_dates)) %>%
   tidyr::separate(file_name, c("start_date", "end_date"), sep = "_to_")
 
-# Count sum and has (yes/no) of consultations grouped by age group
-# We could add more variables to group by here, e.g., ethnicity or sex
-# df_summary <- consultation_datasets %>%
-#   dplyr::group_by(start_date, end_date, age_greater_equal_65) %>%
-#   dplyr::summarise(
-#     n_sum_f2f = sum(count_f2f_consultation, na.rm = TRUE),
-#     n_sum_virtual = sum(count_virtual_consultation, na.rm = TRUE),
-#     n_sum_appointment = sum(count_appointments, na.rm = TRUE),
-#     n_has_f2f = sum(has_f2f_consultation, na.rm = TRUE),
-#     n_has_virtual = sum(has_virtual_consultation, na.rm = TRUE),
-#     n_has_appointment = sum(has_appointments, na.rm = TRUE),
-#   ) %>%
-#   pivot_longer(
-#     cols = c(
-#       n_sum_f2f, n_sum_virtual, n_sum_appointment,
-#       n_has_f2f, n_has_virtual, n_has_appointment
-#     ),
-#     names_to = c("summary_type", "consultation_type"),
-#     values_to = "value", names_sep = "_(?=f2f|virtual|appointment)"
-#   )
 
 #Count sum and has (yes/no) of consultations grouped by age group
 #We could add more variables to group by here, e.g., ethnicity or sex
