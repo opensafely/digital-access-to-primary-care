0e9d23c3dbcbf16bd78fda1e91d09692b2918db7
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-01-08 18:23:55 +0100

    Improve binary regression script
    The results now go into a subfolder called results and are all written to one file.

--- analysis/binary_regression.R
+++ analysis/binary_regression.R
@@ -8,10 +8,7 @@ library(tidyr)
 library(purrr)
 
 # Read file paths of all datasets
-consultation_dataset_paths <- fs::dir_ls(
-  path = "output/",
-  glob = "*consultation_dataset_*.arrow$"
-)
+consultation_dataset_paths <- fs::dir_ls(path = "output/", glob = "*consultation_dataset_*.arrow$")
 
 # Define regex that extracts the date range from the filename
 regex_get_dates <- "\\d{4}-\\d{2}-\\d{2}_to_\\d{4}-\\d{2}-\\d{2}"
@@ -27,20 +24,22 @@ consultation_datasets <- consultation_dataset_paths %>%
 consultation_datasets_filtered <- consultation_datasets %>% filter(start_date == "2019-04-01")
 
 # Run a generalised linear model with binary outcome using has_virtual_consultation as outcome
-summary_text <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
+summary_text1 <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
 
 # filter by start date to separate the data to financials years 2020/21
 consultation_datasets_filtered <- consultation_datasets %>% filter(start_date == "2020-04-01")
 
 # Run a generalised linear model with binary outcome using has_virtual_consultation as outcome
-summary_text <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
+summary_text2 <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
 
 # filter by start date to separate the data to financials years 2021/22
 consultation_datasets_filtered <- consultation_datasets %>% filter(start_date == "2021-04-01")
 
 # Run a generalised linear model with binary outcome using has_virtual_consultation as outcome
-summary_text <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
+summary_text3 <- capture.output(summary(glm(has_virtual_consultation ~ age_greater_equal_65 + sex + imd_quintile + ethnicity, data = consultation_datasets_filtered, family = "binomial")))
 
 # Write data
-fs::dir_create(here::here("output", "data"))
-write_lines(summary_text, here("output", "data", "binary_regression.txt"))
+fs::dir_create(here::here("output", "results"))
+write_lines(c("start_date == '2019-04-01'", summary_text1), here("output", "results", "binary_regression.txt"))
+write_lines(c("start_date == '2020-04-01'", summary_text2), here("output", "results", "binary_regression.txt"), append = TRUE)
+write_lines(c("start_date == '2021-04-01'", summary_text3), here("output", "results", "binary_regression.txt"), append = TRUE)
