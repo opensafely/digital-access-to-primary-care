7108dc27d2c20d188b527a4fb0dd25096b319cfa
Milan Wiedemann <milan.wiedemann@gmail.com>
2024-01-09 11:34:45 +0100

    Add summary for datasets

--- analysis/summarise_consultation_dataset.R
+++ analysis/summarise_consultation_dataset.R
@@ -24,29 +24,35 @@ consultation_datasets <- consultation_dataset_paths %>%
   tidyr::separate(file_name, c("start_date", "end_date"), sep = "_to_")
 
 
-#Count sum and has (yes/no) of consultations grouped by age group
-#We could add more variables to group by here, e.g., ethnicity or sex
+# Count sum and has (yes/no) of consultations grouped by age group
+# We could add more variables to group by here, e.g., ethnicity or sex
 df_summary <- consultation_datasets %>%
-  dplyr::group_by(start_date, end_date, age_greater_equal_65, sex, imd_quintile) %>%
+  dplyr::group_by(start_date, end_date, age_greater_equal_65) %>%
   dplyr::summarise(
-    n_has_appointment = sum(has_appointment, na.rm = TRUE),
-    n_has_virtual = sum(has_virtual_consultation, na.rm = TRUE),
+    has_appointments = sum(has_appointment, na.rm = TRUE),
+    has_virtual = sum(has_virtual_consultation, na.rm = TRUE),
+    has_f2f = sum(has_f2f_consultation, na.rm = TRUE),
+    count_appointments = sum(count_appointment, na.rm = TRUE),
+    count_virtual = sum(count_virtual_consultation, na.rm = TRUE),
+    count_f2f = sum(count_f2f_consultation, na.rm = TRUE),
   ) %>%
+  ungroup() %>%
   pivot_longer(
-    cols = c(n_has_appointment, n_has_virtual),
+    cols = c(has_appointments, has_virtual, has_f2f, count_appointments, count_virtual, count_f2f),
     names_to = "summary_type",
     values_to = "value"
-  )
-
-# Apply disclosure controls
-# Redact values lower or equal 7 and round all other values to nearest 10
-df_summary <- df_summary %>%
+  ) %>%
+  # Apply disclosure controls
+  # Redact values lower or equal 7 and round all other values to nearest 10
   mutate(value = case_when(
     value <= 7 ~ NA_real_,
     TRUE ~ round(value, -1)
-  ))
+  )) %>%
+  separate(summary_type, into = c("method", "data_source")) %>%
+  pivot_wider(names_from = data_source, values_from = value) %>%
+  arrange(start_date, age_greater_equal_65, method)
 
 # Write data
 # Convert all NAs to show "(Redacted)"
 fs::dir_create(here::here("output", "data"))
-write_csv(df_summary, here::here("output", "data", "summary_consultation_dataset.csv"), na = "(Redacted)")
+write_csv(df_summary, here::here("output", "data", "summary_consultation_datasets.csv"), na = "(Redacted)")
